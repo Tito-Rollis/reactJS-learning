5a0dd8a4a9f231d22935200deeeb54794f549f30
Tito-Rollis <ouvalres@gmail.com>
2021-03-27 17:23:06 +0700

    add new folder Person and nest it within App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import React, {Component} from "react";
+// Inporting Person.js
+import Person from "./Person/Person";
 
 // If you use function, just use return, not render method instead
 // function App() {
@@ -10,25 +12,27 @@ import React, {Component} from "react";
 // 	);
 // }
 
-// If you want to use render method, use Class instead of function
-// NOTE : You have only use one Container, so if you want to add new elements, put it in one container
+// You can also use createElement method for returning
+// first parameter is for the HTML element, second is for props (use null if no props), third is either be the string or new element child element (if elemenet use React.createElement())
 // class App extends Component {
 // 	render() {
-// 		return (
-// 			<div className="App">
-// 				<h1>Hello World</h1>
-// 			</div>
+// 		return React.createElement(
+// 			"div",
+// 			{className: "App"},
+// 			React.createElement("h1", null, "Hello World")
 // 		);
 // 	}
 // }
-// You can also use createElement method for returning
-// first parameter is for the HTML element, second is for props (use null if no props), third is either be the string or new element child element (if elemenet use React.createElement())
+
+// If you want to use render method, use Class instead of function
+// NOTE : You have only use one Container, so if you want to add new elements, put it in one container
 class App extends Component {
 	render() {
-		return React.createElement(
-			"div",
-			{className: "App"},
-			React.createElement("h1", null, "Hello World")
+		return (
+			<div className="App">
+				<h1>Hello World</h1>
+				<Person />
+			</div>
 		);
 	}
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+const person = () => {
+	return <h1>Hey, my name is Tito Rollis</h1>;
+};
+
+export default person;
