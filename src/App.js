a10be5202d2537497c2622fe9a8d818f8f82b2ea
Tito-Rollis <ouvalres@gmail.com>
2021-03-27 17:07:41 +0700

    add notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
-import {render} from "@testing-library/react";
-import {Component} from "react";
+import React, {Component} from "react";
+
+// If you use function, just use return, not render method instead
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			<h1>Hello World</h1>
+// 		</div>
+// 	);
+// }
 
 // If you want to use render method, use Class instead of function
+// NOTE : You have only use one Container, so if you want to add new elements, put it in one container
 // class App extends Component {
 // 	render() {
 // 		return (
@@ -13,13 +21,16 @@ import {Component} from "react";
 // 		);
 // 	}
 // }
-// If you use function, just use return, not render method instead
-function App() {
-	return (
-		<div className="App">
-			<h1>Hello World</h1>
-		</div>
-	);
+// You can also use createElement method for returning
+// first parameter is for the HTML element, second is for props (use null if no props), third is either be the string or new element child element (if elemenet use React.createElement())
+class App extends Component {
+	render() {
+		return React.createElement(
+			"div",
+			{className: "App"},
+			React.createElement("h1", null, "Hello World")
+		);
+	}
 }
 
 export default App;
