db171aa45561cdf9d9d31b843b597fc325779a95
Tito-Rollis <ouvalres@gmail.com>
2021-03-27 16:34:18 +0700

    given notes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import "./App.css";
 import {render} from "@testing-library/react";
 import {Component} from "react";
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<h1>Hello World</h1>
-			</div>
-		);
-	}
+// If you want to use render method, use Class instead of function
+// class App extends Component {
+// 	render() {
+// 		return (
+// 			<div className="App">
+// 				<h1>Hello World</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+// If you use function, just use return, not render method instead
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+		</div>
+	);
 }
 
 export default App;
